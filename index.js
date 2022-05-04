8bbd5889ad2bb811ad8e297d31282dd3f0e2e067
xSeanxMillerx <xseanxmillerx@gmail.com>
2022-05-04 14:12:35 -0700

    Update files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// ------------------------------------------------------------------------------------------
+
 // computerPlay() : to make the computer randomly return 'rock', 'paper' or 'scissors'
 const computerPlay = () => {
     // computerChoice is an array with the different choices possible
@@ -16,101 +16,41 @@ const computerPlay = () => {
     return computerSelection = 'scissors'
     }
 }
-// console.log(computerPlay()) ---> prints: 'rock', 'paper' or 'scissors'
-
 // ------------------------------------------------------------------------------------------
+
 // playerPlay : to make the player choose between 'rock', 'paper' or 'scissors'
 const playerPlay = () => {
-// to display a dialog and let the player type his choice between "rock", "paper", or "scissors"
-// ---> DECOCHER let inputPlayer = prompt('Type "rock", "paper", or "scissors"')
-// to make inputPlayer case insensitive (to compare with computerSelection easily)
-// ---> DECOCHER let playerSelection = inputPlayer.toLocaleLowerCase();
-// playerSelection will store the return value of playerPlay()
 return playerSelection 
 }
-// console.log(playerPlay()) ---> prints: 'rock', 'paper' or 'scissors'
 
 // ------------------------------------------------------------------------------------------
+
 // playRound() : to play a single round
 const playRound = (playerSelection, computerSelection) => {    
-// use the return value of computerPlay() and use it as first argument
-// ---> DECOCHER computerSelection = computerPlay();
-// use the return value of playerPlay() and use it as second argument
 playerSelection = playerPlay();
 
-// roundWinner will be use to store the return value of playRound()
-// ---> DECOCHER let roundWinner = "";
-
-    // All the conditions where the round = tie
-    if (playerSelection === computerSelection) {
-        // Logs to resume round
-  
+    if (playerSelection === computerSelection) {        
         return roundWinner = 'tie'
-    // All the conditions where the player wins the round    
     } else if ((playerSelection === 'rock' && computerSelection === 'scissors') 
     || (playerSelection === 'paper' && computerSelection === 'rock') 
     || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-         // Logs to resume round
-      
-        return roundWinner = 'player'
-    // Others conditions (where computer wins the round)
+    return roundWinner = 'player'
+    
     } else {
-         // Logs to resume round
-        
         return roundWinner = 'computer'
        }       
 }
-// console.log(playRound()) ---> prints: 'tie', 'player' or 'computer'
 
 // ------------------------------------------------------------------------------------------
-// function playGame : to play a five rounds game
-const playGame = () => {
-// To store points/ties 
 
-console.log('------------------------ NEW GAME ------------------------');
-
-    // To play 5 rounds
-    for (let i = 1; i <= 5; i++) {
-    console.log(`‣ Round ${i}`);
-
-    // <xxx>Points will be use to compare and count points
-    switch(playRound()) {
-        case 'player':
-            playerPoints ++;
-            break;
-        case 'computer':
-            computerPoints ++;
-            break;
-        case 'tie': 
-            tiePoints ++;
-            break;   
-        }  
-}
-    // Logs to resume the game
-    console.log(`‣ RESULTS :`);
-    console.log(`Player points: ${playerPoints}`);
-    console.log(`Computer points: ${computerPoints}`);
-    console.log(`Ties: ${tiePoints}`);
-
-    // Log to announce the winner depending on points
-    if (playerPoints > computerPoints) {
-        console.log('🟢 You won the match! 🟢');
-    } else if (playerPoints < computerPoints) {
-        console.log('🔴 You loose the match! 🔴');
-    } else {
-        console.log('🟠 Tie! 🟠');
-    }
-}
-// playGame() 
-
-// ---------------------------------------------------------------------------
 let numberOfRound = 0;
 let playerPoints = 0;
 let computerPoints = 0;
 let tiePoints = 0;
 
-// ---------------------------------------------------------------------------
-// Play rock
+// ------------------------------------------------------------------------------------------
+
+// Play rock 
 const playRockRound = document.querySelector('.rock')
 playRockRound.addEventListener('click', () => {
 
@@ -226,6 +166,8 @@ playRockRound.addEventListener('click', () => {
         
     }        
 }, capture = true)
+
+// ------------------------------------------------------------------------------------------
 
 // Play paper
 const playPaperRound = document.querySelector('.paper')
@@ -344,6 +286,8 @@ playPaperRound.addEventListener('click', () => {
     }        
 }, capture = true)
 
+// ------------------------------------------------------------------------------------------
+
 // Play scissors
 const playScissorsRound = document.querySelector('.scissors')
 playScissorsRound.addEventListener('click', () => {
@@ -461,7 +405,7 @@ playScissorsRound.addEventListener('click', () => {
     }        
 }, capture = true)
 
-// --------------------------------------------
+// ------------------------------------------------------------------------------------------
 
 // New game
 const newGame = document.querySelector('.startBtn')
